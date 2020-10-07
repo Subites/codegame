440a9c833b101bd7dbe7bb2377d75c7dddeeba8d
Subites <72504793+Subites@users.noreply.github.com>
2020-10-07 23:21:47 +0800

    代码优化
    切片优化

--- gamestart.py
+++ gamestart.py
@@ -2,7 +2,7 @@ import maps
 import camera
 import UI
 from time import *
-mainmap = maps.Maps((130,50),"o")#实例化地图
+mainmap = maps.Maps((150,50),"o")#实例化地图
 totalmap = mainmap.mapreset()#重置地图
 maincamera = camera.Camera(mainmap.mapsize,mainmap.mapsize)
 UI.reset()
